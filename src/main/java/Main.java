899177309807a49e3731c2f4c90201b3bb21d7a3
Yakov Medvedev <medvedev.teror@gmail.com>
2019-07-29 12:30:27 +0300

    First commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by medvedevyakov on 2019-07-29.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String ,String> m = new HashMap<>();
+    }
+}
--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,6 +1,17 @@
 package de.comparus.opensource.longmap;
 
 public class LongMapImpl<V> implements LongMap<V> {
+
+    long[] mapSize;
+
+    public LongMapImpl() {
+        mapSize = new long[16];
+    }
+
+    public LongMapImpl(int initSize) {
+        mapSize = new long[initSize];
+    }
+
     public V put(long key, V value) {
         return null;
     }
